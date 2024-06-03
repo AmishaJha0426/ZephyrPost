e504a5324309fad97812519492d90966a5ffdd1a
Amisha <amishajha0426@gmail.com>
2024-06-03 11:21:53 +0530

    add delete user functionality to the admin user

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -4,8 +4,8 @@ const { verifyToken } = require('../utils/verifyUser.js');
 const router=express.Router();
 
 router.get('/test', test);
-router.put("/update/:userID", verifyToken, updateUser);
-router.delete("/delete/:userID", verifyToken, deleteUser);
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers",verifyToken,getUsers);
 
