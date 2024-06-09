625d9f9d9ad3a503d283d576c754cd3401c8d4d2
Amisha <amishajha0426@gmail.com>
2024-06-09 14:56:44 +0530

    complete about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About ZephyrPost
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia alias ducimus excepturi fuga! Dolorem consequatur eum nisi, at quisquam illo vitae esse minima aperiam? Temporibus non dicta recusandae rerum error.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nulla aliquid ipsa eum, consequuntur enim excepturi? Est dolore qui minus unde officiis voluptates! Cupiditate magni vel ea sed doloribus atque!
+            </p>
+
+            <p>
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores omnis in eaque illum at, ducimus a officia excepturi laudantium incidunt dignissimos sit doloribus. Sed sit quibusdam omnis repellendus nulla sunt dolore inventore temporibus. Vel minus quibusdam vitae esse possimus.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
